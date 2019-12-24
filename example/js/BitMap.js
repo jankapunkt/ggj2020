6b34efec261f665c98f26397cf08797f724c1236
jankapunkt <jkuester@uni-bremen.de>
2019-12-24 01:06:04 +0100

    deleted old files from examples

--- example/js/BitMap.js
+++
@@ -1,6 +0,0 @@
-function Bitmap (src, width, height) {
-  this.image = new window.Image()
-  this.image.src = src
-  this.width = width
-  this.height = height
-}
--- example/js/Constants.js
+++
@@ -1,2 +0,0 @@
-Math.PI_HALF = Math.PI / 2
-Math.PI_QUARTER = Math.PI / 4
